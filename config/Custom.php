0ec8ff84f45b186d25f65e3f9f6e9f29e5f17638
quynh <phamquynh047@gmail.com>
2019-10-23 00:55:41 +0700

    config

---
+++ config/Custom.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        'statusPublic' => '1',
+        'statusDraft' => '0',
+        'normalAcconut' => '0',
+        'vipAcconut' => '1',
+        'active' => '1',
+        'deactive' => '0',
+    ];
